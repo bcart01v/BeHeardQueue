00e26789abb7b800dc3265e84e9edd08c0b0b27a
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2025-03-31 21:21:03 -0500

    Fixed blank pages so we can create initial build

--- app/tv-display/page.tsx
+++ app/tv-display/page.tsx
@@ -1,1 +1,8 @@
 // Client side. Would need to be Client side for real time updates
+export default function TVDisplayPage() {
+    return (
+      <div>
+        <h1>TV Display</h1>
+      </div>
+    );
+  }
